b211b5b7f9e9402100cd80aa79cd1d72351dd01b
AnnSyh <ann.syharevskaya@gmail.com>
2022-11-21 17:40:54 +0300

    add type :any for children

--- components/MainLayout.tsx
+++ components/MainLayout.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Head from 'next/head'
 
-export default function MainLayout({ children, title = 'Next App' }) {
+export default function MainLayout({ children, title = 'Next App' }:any) {
   return (
     <>
       <Head>
