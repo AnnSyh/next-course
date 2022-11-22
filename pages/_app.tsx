85c116ea5c79a88217fa636171105a36b62f79c8
AnnSyh <ann.syharevskaya@gmail.com>
2022-11-22 15:31:05 +0300

    commit NextNProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import NextNProgress from 'nextjs-progressbar';
+// import NextNProgress from 'nextjs-progressbar';
 import '../styles/globals.scss';
 import variables from '../styles/variables.module.scss';
 
@@ -6,13 +6,13 @@ import variables from '../styles/variables.module.scss';
 export default function App({ Component, pageProps }:any) {
   return (
     <div style={{ color: variables.primaryColor }}>
-      <NextNProgress
+      {/* <NextNProgress
         color="#64ff00"
         startPosition={0.3}
         stopDelayMs={200}
         height={3}
         showOnShallow={true}
-      />
+      /> */}
       <Component {...pageProps} />
     </div>
   )
